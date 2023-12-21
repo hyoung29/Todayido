f91de4514d4982a70deae45a1d32e7433a34c960
user <rlawogml0510@naver.com>
2023-12-21 16:27:31 +0900

    마이페이지, 리뷰작성

--- src/main/java/com/metamong/todayido/controller/MemberController.java
+++ src/main/java/com/metamong/todayido/controller/MemberController.java
@@ -2,12 +2,14 @@ package com.metamong.todayido.controller;
 
 import com.metamong.todayido.dto.MemberDto;
 import com.metamong.todayido.service.MemberService;
+import com.metamong.todayido.service.detailService;
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -15,6 +17,9 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 public class MemberController {
     @Autowired
     private MemberService mServ;
+
+    @Autowired
+    private detailService dServ;
 
     @GetMapping("/")
     public String home(){
@@ -58,5 +63,21 @@ public class MemberController {
         log.info("logout()");
         String view = mServ.logout(session);
         return view;
+    }
+    @GetMapping("detail")
+    public String detail(){
+        log.info("detail()");
+        return "detail";
+    }
+//    @GetMapping("detail")
+//    public ModelAndView detail(int store_num){
+//        log.info("detail() : {}", store_num);
+//        ModelAndView mv = dServ.getReview(store_num);
+//        return mv;
+//    }
+    @GetMapping("myPage")
+    public String myPage(){
+        log.info("myPage()");
+        return "myPage";
     }
 }
---
+++ src/main/java/com/metamong/todayido/service/detailService.java
@@ -0,0 +1,32 @@
+package com.metamong.todayido.service;
+
+import com.metamong.todayido.dao.detailDao;
+import com.metamong.todayido.dto.reviewDto;
+import com.metamong.todayido.dto.storeDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Service
+@Slf4j
+public class detailService {
+    @Autowired
+    private detailDao dDao;
+
+    public ModelAndView getReview(int store_num){
+        log.info("getReview()");
+        ModelAndView mv = new ModelAndView();
+        List<storeDto> sList = dDao.seleteStore(store_num);
+        mv.addObject("sList", sList);
+        List<reviewDto> rList = dDao.seletereview(store_num);
+        mv.addObject("rList", rList);
+
+        mv.setViewName("detail");
+        return mv;
+    }
+
+
+}
